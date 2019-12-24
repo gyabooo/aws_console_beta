7edfe4525ba21ccf776fecd67d4db9b5571119ba
seita_matsumoto <gyabooo@gmail.com>
2019-12-24 20:51:04 +0900

    deviseの不要な機能削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
 
   has_many :organization_users
   has_many :organizations, through: :organization_users
